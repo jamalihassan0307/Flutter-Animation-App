e5bd6e8feeaa54b6bcbe4987fb4bcb730a0afa9f
jamalihassan0307 <jamalihassan0307@gmail.com>
2023-10-07 00:28:10 +0500

    first commit

---
+++ lib/model/homelist.dart
@@ -0,0 +1,31 @@
+import 'package:animation_app/fitness_app/fitness_app_home_screen.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeList {
+  HomeList({
+    this.navigateScreen,
+    this.imagePath = '',
+  });
+
+  Widget? navigateScreen;
+  String imagePath;
+
+  static List<HomeList> homeList = [
+    // HomeList(
+    //   imagePath: 'assets/introduction_animation/introduction_animation.png',
+    //   navigateScreen: IntroductionAnimationScreen(),
+    // ),
+    // HomeList(
+    //   imagePath: 'assets/hotel/hotel_booking.png',
+    //   navigateScreen: HotelHomeScreen(),
+    // ),
+    HomeList(
+      imagePath: 'assets/fitness_app/fitness_app.png',
+      navigateScreen: const FitnessAppHomeScreen(),
+    ),
+    // HomeList(
+    //   imagePath: 'assets/design_course/design_course.png',
+    //   navigateScreen: DesignCourseHomeScreen(),
+    // ),
+  ];
+}
---
+++ lib/navigation_home_screen.dart
@@ -0,0 +1,78 @@
+import 'package:animation_app/custom_drawer/apptheme.dart';
+import 'package:animation_app/custom_drawer/drawer_user_controller.dart';
+import 'package:animation_app/custom_drawer/home_drawer.dart';
+import 'package:animation_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  const NavigationHomeScreen({super.key});
+
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget? screenView;
+  DrawerIndex? drawerIndex;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const MyHomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.white,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+              //callback from drawer for replace screen as user need with passing DrawerIndex(Enum index)
+            },
+            screenView: screenView,
+            //we replace screen view as we need on navigate starting screens like MyHomePage, HelpScreen, FeedbackScreen, etc...
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      switch (drawerIndex) {
+        case DrawerIndex.HOME:
+          setState(() {
+            screenView = const MyHomePage();
+          });
+          break;
+        case DrawerIndex.Help:
+          setState(() {
+            screenView = const MyHomePage();
+          });
+          break;
+        case DrawerIndex.FeedBack:
+          setState(() {
+            screenView = const MyHomePage();
+          });
+          break;
+        case DrawerIndex.Invite:
+          setState(() {
+            screenView = const MyHomePage();
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
