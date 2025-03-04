2fa139e2d8f50f1b569d406a7bb600efdbfbdce9
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-04 18:01:44 +0500

    add profile image

--- lib/custom_drawer/home_drawer.dart
+++ lib/custom_drawer/home_drawer.dart
@@ -109,7 +109,7 @@ class _HomeDrawerState extends State<HomeDrawer> {
                             child: ClipRRect(
                               borderRadius:
                                   const BorderRadius.all(Radius.circular(60.0)),
-                              child: Image.asset('assets/images/userImage.png'),
+                              child: Image.asset('assets/images/userImage.jpg'),
                             ),
                           ),
                         ),
